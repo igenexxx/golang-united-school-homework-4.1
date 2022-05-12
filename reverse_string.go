9d3b0df91228bb4037ba9dac3b083633b52b9b0c
Zhenya Sikirzhitsky <zhenya_sikirzhitsky@epam.com>
2022-05-12 19:17:21 +0400

    feature/reverse: complete string reverse

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,19 @@
 package reverse_string
 
+func reverse(input string) string {
+	var result []uint8
+	length := len(input) - 1
+
+	for i, _ := range input {
+		result = append(result, input[length-i])
+	}
+
+	return string(result)
+}
+
 func ReverseString(input string) (output string) {
 	// solution goes here
-	return output
+	output = reverse(input)
+
+	return
 }
